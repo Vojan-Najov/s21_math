e243287666ac88d7c2275908323052cca05909b9
Vojan-Najov <serejah.mashichev@gmail.com>
2023-08-28 20:22:45 +0300

    Update headers.

--- include/s21_math.h
+++ include/s21_math.h
@@ -16,21 +16,23 @@ int s21_abs(int x);
 long double s21_acos(double x);
 long double s21_asin(double x);
 long double s21_atan(double x);
-double s21_ceil(double x);
-double s21_cos(double x);
+long double s21_ceil(double x);
+long double s21_cos(double x);
 long double s21_exp(double x);
-double s21_fabs(double x);
-double s21_floor(double x);
-double s21_fmod(double x, double y);
-double s21_log(double x);
+long double s21_fabs(double x);
+long double s21_floor(double x);
+long double s21_fmod(double x, double y);
+long double s21_log(double x);
 long double s21_pow(double base, double exp);
-double s21_sin(double x);
-double s21_sqrt(double x);
-double s21_tan(double x);
-long double s21_trunc(double num);
+long double s21_sin(double x);
+long double s21_sqrt(double x);
+long double s21_tan(double x);
 
+/* auxiary functions */
+long double s21_trunc(double num);
 int s21_isnan(double x);
 int s21_isinf(double x);
 float _s21_nan(void);
 float _s21_inf(void);
+long double _s21_integer_pow(double base, double e);
 #endif
